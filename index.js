285c5e66534964d51a7fc8a546b5ea819b55914d
harsh shrivastava <harshshrivastava@gmail.com>
2020-07-20 22:46:50 +0530

    adding sass middle ware

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ const session = require('express-session');
 const passport = require('passport');
 const passportLocal =require('./config/passport-local-strategy');
 const MongoStore = require('connect-mongo')(session);
+const sassMiddleWare = require('node-sass-middleware');
 
 app.use(express.urlencoded());
 app.use(cookieParser());
